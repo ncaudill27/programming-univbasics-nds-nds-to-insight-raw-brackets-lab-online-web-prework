ebd6789d84e583932f5cd4b395a44753c5b85289
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-20 17:24:20 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,24 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+	result = {}
+	# First we must iterate through an Array of Hashes
+	outer_shell = 0
+	while outer_shell < nds.length do
+		# Now that we have the Hashes. We can now access :name and :movies, the latter being an AoH.
+		# Then we begin to iterate through the :movies tag paired alongside :name, remember, this tag points to an AoH.
+		director = nds[outer_shell][:name] # Save the :name location.
+		iter_movies = 0
+		gross_values = [] # Vessel for :worldwide_gross.
+		while iter_movies < nds[outer_shell][:movies].length do
+			# Finally we have access to the lowest level keys(:title,:studio,:worldwide_gross,:release_year)
+			inner_key = nds[outer_shell][:movies][iter_movies] # Save "key" to access other values if needed.
+			gross_values << inner_key[:worldwide_gross] # Shovel values into vessel for extraction.
+			iter_movies += 1
+		end
+		# Add saved director location and values vessel into Hash form. Preparing for return.
+		result.store(director, gross_values.sum)
+		outer_shell += 1
+	end
+	result
 end
